f4159b7601318e2d25b770313739d0dffcf8fd2c
victormdev <victoralexandrino99@gmail.com>
2021-10-24 17:58:54 -0300

    script para detecção de olhos

---
+++ SimpleImage.py
@@ -0,0 +1,23 @@
+import cv2
+
+# Carregando xml responsável pela identidicação da função/imagem
+loadScript = cv2.CascadeClassifier('haarcascades/haarcascade_frontalface_default.xml')
+
+# Importando imagem
+image = cv2.imread('images/imagem1.jpg')
+
+# Deixando imagem cinza
+imageGray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+faces = loadScript.detectMultiScale(imageGray, scaleFactor=1.05)
+
+# Mostrando as matrizes
+print(faces)
+
+for(x, y, l, a) in faces:
+    # Criando retângulo definindo as propriedades (eixo x e y, cor e largura da borda)
+    cv2.rectangle(image, (x, y), (x + l, y + a), (0, 255, 0), 2)
+
+# Mostrando a imagem em popup
+cv2.imshow("Faces", image)
+cv2.waitKey()
